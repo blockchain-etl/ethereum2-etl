cc5f020955bcd6093ba4753a6f585fed0dd4c786
medvedev1088 <medvedev1088@gmail.com>
2020-11-27 12:06:37 +0700

    Bump version

--- ethereum2etl/cli/__init__.py
+++ ethereum2etl/cli/__init__.py
@@ -29,7 +29,7 @@ from ethereum2etl.cli.get_epoch_range_for_date import get_epoch_range_for_date
 
 
 @click.group()
-@click.version_option(version='0.0.6')
+@click.version_option(version='0.0.7')
 @click.pass_context
 def cli(ctx):
     pass
